5da624b7be34739ae3d97bae4d6cee49eb4f2f22
Nikola Mirkovic <nikola.mirkovic.bg@gmail.com>
2023-11-25 16:24:51 +0100

    Added Spinner and RadioButtonWithText

--- POSAplikacija/app/src/main/java/com/example/posaplikacija/MainActivity.kt
+++ POSAplikacija/app/src/main/java/com/example/posaplikacija/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.posaplikacija
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,6 +13,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.posaplikacija.ui.theme.POSAplikacijaTheme
+import rs.etf.running.ui.elements.composables.RadioButtonWithText
+import rs.etf.running.ui.elements.composables.Spinner
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +26,33 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    POSApplication()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    POSAplikacijaTheme {
-        Greeting("Android")
+fun POSApplication()
+{
+    var selected: Boolean = false
+    var options = listOf<String>("Abc", "Xyz", "Pqr")
+    Column()
+    {
+        Row()
+        {
+
+            RadioButtonWithText(text = "Test", selected = selected, onClick = { /*TODO*/ })
+        }
+        Row()
+        {
+            Spinner(value = "Random lista", onSelect = {}, options = options)
+        }
     }
+
+
+
+
 }
